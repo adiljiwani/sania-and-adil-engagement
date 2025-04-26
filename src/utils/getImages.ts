d123f02aa18dd1fe72955d162c12212f06465516
Adil Jiwani <ajiwani@seatgeek.com>
2025-04-26 16:26:19 -0400

    quicker with webp

--- src/utils/getImages.ts
+++ src/utils/getImages.ts
@@ -6,39 +6,39 @@ export interface Image {
 export function getImages(): Image[] {
   return [
     {
-      src: '/images/250224_adil_0003.jpg',
+      src: '/images/250224_adil_0003.webp',
       alt: 'Engagement photo 1',
     },
     {
-      src: '/images/250224_adil_0007.jpg',
+      src: '/images/250224_adil_0007.webp',
       alt: 'Engagement photo 2',
     },
     {
-      src: '/images/250224_adil_0017.jpg',
+      src: '/images/250224_adil_0017.webp',
       alt: 'Engagement photo 3',
     },
     {
-      src: '/images/250224_adil_0031.jpg',
+      src: '/images/250224_adil_0031.webp',
       alt: 'Engagement photo 4',
     },
     {
-      src: '/images/250224_adil_0039.jpg',
+      src: '/images/250224_adil_0039.webp',
       alt: 'Engagement photo 5',
     },
     {
-      src: '/images/250224_adil_0101.jpg',
+      src: '/images/250224_adil_0101.webp',
       alt: 'Engagement photo 6',
     },
     {
-      src: '/images/250224_adil_0136.jpg',
+      src: '/images/250224_adil_0136.webp',
       alt: 'Engagement photo 7',
     },
     {
-      src: '/images/250224_adil_0187.jpg',
+      src: '/images/250224_adil_0187.webp',
       alt: 'Engagement photo 8',
     },
     {
-      src: '/images/250224_adil_0230.jpg',
+      src: '/images/250224_adil_0230.webp',
       alt: 'Engagement photo 9',
     }
   ];
